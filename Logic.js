a9d6de4e97067ea3c71e52fd753f010e5922c042
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-07-11 08:12:34 +0530

    Used switch stms logic insted of if stmt

--- Logic.js
+++ Logic.js
@@ -8,17 +8,22 @@ btn.forEach( (b1)=>{
     b1.addEventListener('click',(eve)=>{
         // console.log(eve);
         console.log(eve.target.id);  //one, tow, three, four
-        if (eve.target.id==='one') {
-            body.style.backgroundColor = 'pink'
-        }
-        if (eve.target.id==='tow') {
-            body.style.backgroundColor = 'skyblue'
-        }
-        if (eve.target.id==='three') {
-            body.style.backgroundColor = 'black'
-        }
-        if (eve.target.id==='four') {
-            body.style.backgroundColor = 'violet'
+        switch (eve.target.id) {
+            case 'one':
+                body.style.backgroundColor = 'pink';
+                break;
+            case 'tow':
+                body.style.backgroundColor = 'skyblue';
+                break;
+            case 'three':
+                body.style.backgroundColor = 'grey';
+                break;
+            case 'four':
+                body.style.backgroundColor = 'violet';
+                break;
+        
+            default:
+                break;
         }
     })
 });
